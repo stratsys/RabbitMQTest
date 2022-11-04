7213f49b503e739928a8a26ff576a08e9d2d10af
Sebastian Lind <sebastian.lind@stratsys.se>
2022-11-04 15:40:13 +0100

    stuff

--- src/main.go
+++ src/main.go
@@ -1,76 +1,24 @@
 package main
 
 import (
-	"log"
-
-	amqp "github.com/rabbitmq/amqp091-go"
+	"flag"
+	"fmt"
+	"main/consumer"
+	"main/producer"
+	"os"
 )
 
-func failOnError(err error, msg string) {
-	if err != nil {
-		log.Panicf("%s: %s", msg, err)
-	}
-}
-
 func main() {
-	conn, err := amqp.Dial("amqp://guest:guest@rabbitmq/")
-	failOnError(err, "Failed to connect to RabbitMQ ##")
-	defer conn.Close()
 
-	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
+	var input string
+	flag.StringVar(&input, "mode", "consum", "run as consum or produce")
+	flag.Parse()
+	if input == "producer" {
+		producer.InitProducer()
+	} else {
+		consumer.InitConsumer()
+	}
+	fmt.Println("Shutdowm")
+	os.Exit(0)
 
-	err = ch.ExchangeDeclare(
-		"demo.exchange", // name
-		"fanout",        // type
-		true,            // durable
-		false,           // auto-deleted
-		false,           // internal
-		false,           // no-wait
-		nil,             // arguments
-	)
-	failOnError(err, "Failed to declare an exchange")
-
-	q, err := ch.QueueDeclare(
-		"demo.queue", // name
-		false,        // durable
-		false,        // delete when unused
-		false,        // exclusive
-		false,        // no-wait
-		nil,          // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
-
-	err = ch.QueueBind(
-		q.Name,          // queue name
-		"",              // routing key
-		"demo.exchange", // exchange
-		false,
-		nil,
-	)
-	failOnError(err, "Failed to bind a queue")
-
-	msgs, err := ch.Consume(
-		q.Name, // queue
-		"",     // consumer
-		true,   // auto-ack
-		false,  // exclusive
-		false,  // no-local
-		false,  // no-wait
-		nil,    // args
-	)
-	failOnError(err, "Failed to register a consumer")
-
-	var forever chan struct{}
-
-	go func() {
-		for d := range msgs {
-			log.Printf("Received a message: %s", d.Body)
-
-		}
-	}()
-
-	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
-	<-forever
 }
---
+++ src/producer/producer.go
@@ -0,0 +1,61 @@
+package producer
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strconv"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func InitProducer() {
+	conn, err := amqp.Dial("amqp://admin:queueAdmin@queue.dev-backends/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	// q, err := ch.QueueDeclare(
+	// 	"demo.queue", // name
+	// 	false,        // durable
+	// 	false,        // delete when unused
+	// 	false,        // exclusive
+	// 	false,        // no-wait
+	// 	nil,          // arguments
+	// )
+	failOnError(err, "Failed to declare a queue")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var jsonBody string
+
+	for i := 1; i < 100000; i++ {
+		jsonBody = fmt.Sprintf("{'" + strconv.Itoa(i) + "': 'test'}")
+		err = ch.PublishWithContext(ctx,
+			"demo.exchange", // exchange
+			"",              // routing key
+			false,           // mandatory
+			false,           // immediate
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        []byte(jsonBody),
+			})
+		failOnError(err, "Failed to publish a message")
+		log.Printf(" [x] Sent %s\n", jsonBody)
+	}
+	for true {
+		log.Printf(" sleep ")
+		time.Sleep(60 * time.Second)
+
+	}
+}
